c5aa00dee40dea35b518effccaa1e5014deae8fb
Daniel Linge <daniel_linge@yahoo.com>
2016-09-20 16:36:31 -0400

    moved variables inside function

--- mt_monitor.py
+++ mt_monitor.py
@@ -7,10 +7,8 @@ from datetime import datetime
 import slackweb  #pip install slackweb https://github.com/satoshi03/slack-python-webhook/blob/master/README.md
 import requests  #pip install requests
 
-i = datetime.now()
 
 #Slack incoming web hook
-slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
 
 # List of Clients
 clients = {
@@ -34,20 +32,24 @@ clients = {
 
 def mt_site_up():
         "Function to monitor uptime"
-        print("Running check at " + str(i))
+        tlpSlack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+        d = datetime.now()
+        print("Running check at " + str(d))
         for client, site in clients.items():
             try:
                 r = requests.get(site, verify=False, timeout =10)
                 if r.status_code == 200:    # 200 = up! 
                     time.sleep(1)           # 1 second between checks
-                    print( str(client) + " is up at " + str(i))
+                    d = datetime.now()
+                    print( str(client) + " is up at " + str(d))
                     print(r.status_code)
                 else:
-                    slack.notify(text=client + " Website is Down!", channel="#ec2-status", username="status-bot",icon_emoji=':warning:')
-                    print( str(client) + " is down at " + str(i))
+                    tlpSlack.notify(text=client + " Website is Down!", channel="#ec2-status", username="status-bot",icon_emoji=':warning:')
+                    d = datetime.now()
+                    print( str(client) + " is down at " + str(d))
                     print(r.status_code)
             except requests.exceptions.ReadTimeout:
-                slack.notify(text=client + " Website is Down!", channel="#ec2-status", username="status-bot", icon_emoji=':warning:')
+                tlpSlack.notify(text=client + " Website is Down!", channel="#ec2-status", username="status-bot", icon_emoji=':warning:')
             except:
                 pass
 
