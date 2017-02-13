7ac9c16c792113e56736c47d92ed361702bc368d
Daniel Linge <daniel_linge@yahoo.com>
2017-02-13 11:05:36 -0500

    changed url in getip

--- getip.py
+++ getip.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 
-from urllib.request import urlopen
+from requests import get
 
 def getcity():
-    atlip = b'76.17.118.203'
-    myip = urlopen("http://myip.dnsdynamic.org/").read()
+    atlip = '76.17.118.203'
+    myip = str(get("https://api.ipify.org").text)
     if myip == atlip:
         city = "Atlanta"
+        print(city)
         return city
     else:
         city = "Melbourne"
+        print(city)
         return city
 
 
